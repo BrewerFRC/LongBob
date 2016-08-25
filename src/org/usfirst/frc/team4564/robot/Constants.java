a022d047b50e61a3792cb07611cfffe2a61aaa6d
Evan McCoy <qwertynessthedev@gmail.com>
2016-08-25 18:53:40 -0400

    Added Constants and DriveTrain classes.

---
+++ src/org/usfirst/frc/team4564/robot/Constants.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team4564.robot;
+
+public class Constants {
+	
+	//PWM inputs
+	public static final int PWM_DRIVE_L = 0;
+	public static final int PWN_DRIVE_R = 1;
+	
+	//Digital IO
+	
+}
